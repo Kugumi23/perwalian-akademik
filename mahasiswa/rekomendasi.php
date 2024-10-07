ddd7ad3c3fbba21383d2219229608f1cd186d5fa
Jericho <revxcrish236@gmail.com>
2024-10-07 23:38:23 +0700

    D

--- mahasiswa/rekomendasi.php
+++ mahasiswa/rekomendasi.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BSPAM Online : Rekomendasi</title>
-    <link rel="stylesheet" href=".../vendor/twbs/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="../vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style/style.css">
     <style>

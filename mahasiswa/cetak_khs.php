2808db441c00c244a49c8218bd7c20e7e8793f16
Jericho <revxcrish236@gmail.com>
2024-10-07 23:23:05 +0700

    Done

--- mahasiswa/cetak_khs.php
+++ mahasiswa/cetak_khs.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.css">
     <title>BSPAM Online : Cetak KHS</title>
     <link rel="stylesheet" href="style/khs.css">
     <link rel="stylesheet" href="style/style.css">

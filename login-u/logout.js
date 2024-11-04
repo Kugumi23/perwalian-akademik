1fb76fc535084faaa424110333c32aa27ae6d89b
Jericho <revxcrish236@gmail.com>
2024-11-04 14:26:58 +0700

    Penambahan user-id-show pada halaman akun mahasiswa

--- login-u/logout.js
+++ login-u/logout.js
@@ -1,6 +1,6 @@
 document.getElementById("logout").addEventListener("submit", function(event){
     event.preventDefault();
-
+    
     localStorage.removeItem('token');
     localStorage.removeItem('id');
     window.location.href = "../login-u/login-form.html";
